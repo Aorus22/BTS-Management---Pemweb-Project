543469a4c3f0f41bace8b5412ccf4c08a7c0d2ad
aorus22 <alyzanadif04@gmail.com>
2024-07-09 06:42:07 +0700

    small change

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,10 +5,10 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = 'Manajemen Tower BTS';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
