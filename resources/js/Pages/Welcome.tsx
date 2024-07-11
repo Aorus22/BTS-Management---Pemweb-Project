6db07d7b860aee0bc6b93a6c9f74b75934afcb68
aorus22 <alyzanadif04@gmail.com>
2024-07-11 19:54:04 +0700

    fix

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -17,7 +17,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                             {auth.user ? (
                                 <Link
                                     href={route('dashboard')}
-                                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-gray-200/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white text-white"
+                                    className="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-gray-200/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white text-white"
                                 >
                                     Dashboard
                                 </Link>
@@ -25,13 +25,13 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                                 <>
                                     <Link
                                         href={route('login')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-gray-200/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white text-white"
+                                        className="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-gray-200/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white text-white"
                                     >
                                         Log in
                                     </Link>
                                     <Link
                                         href={route('register')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-gray-200/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white text-white"
+                                        className="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-gray-200/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white text-white"
                                     >
                                         Register
                                     </Link>
